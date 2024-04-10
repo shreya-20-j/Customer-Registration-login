880312ce8c11afd9876037ba62c9d81e8dfac0f2
Shreya Johnson <85394165+shreya-20-j@users.noreply.github.com>
2024-04-10 14:45:14 +0530

    Update TabCreate.java

--- TabCreate.java
+++ TabCreate.java
@@ -16,9 +16,50 @@ public class TabCreate {
 			
 			Statement stm = con.createStatement();
 			
-			String sql = "CREATE TABLE customer(Customer_id Integer not NULL PRIMARY KEY AUTO_INCREMENT,Name varchar(20) not NULL,Place varchar(20) not null,Phone_number varchar(15) not null)";
-			stm.executeUpdate(sql);
+			String sql1 = "CREATE TABLE customer ("
+				    + "Customer_id INT , "
+				    + "Name VARCHAR(255) NOT NULL PRIMARY KEY,"
+				    + "Place VARCHAR(255) , "
+				    + "Phone_number VARCHAR(15) UNIQUE) ";
+				
+			String sql2 = "CREATE TABLE product ("
+				    + "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+				    + "product_name VARCHAR(20) , "
+				    + "Price INT(20), "
+				    + "category VARCHAR(20), "
+				    + "description VARCHAR(50), "
+				    + "image BLOB, "
+				    + "stock VARCHAR(10))";
+			
+			
+			
+			String sql3 = "CREATE TABLE orders ("
+			            + "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+			            + "quantity INT , "
+			            + "price INT , "
+			            + "total INT , "
+			            + "name VARCHAR(20), "
+			            + "orderDate VARCHAR(100), "
+			            + "deliveryDate VARCHAR(100) , "
+			            + "paymentMethod VARCHAR(100), "
+			            + "transactionid VARCHAR(30), "
+			            + "status VARCHAR(10) ,"
+			            + "phone_number VARCHAR(20) , "
+			            + "place VARCHAR(100))";
+			
+
+			String sql4 = "CREATE TABLE message ("
+			            + "id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+			            + "name VARCHAR(20) NOT NULL, "
+			            + "subject VARCHAR(200) NOT NULL, "
+			            + "body VARCHAR(1000) NOT NULL)";
+
+			stm.executeUpdate(sql1);
+			stm.executeUpdate(sql2);
+			stm.executeUpdate(sql3);
+			stm.executeUpdate(sql4);
 			System.out.println("table created");
+			
 			}
 			catch(SQLException | ClassNotFoundException e){
 				e.printStackTrace();
